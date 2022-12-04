fd5ed4d8de7a22923812f8d13c05e4ee9c1ea9de
johnclecio <johnclecio@icloud.com>
2022-12-04 20:29:52 -0300

    vuejs 1.0

---
+++ vuejs-cli/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+//import VueCompositionAPI, { h } from '@vue/composition-api'
+//Vue.use(VueCompositionAPI)
+createApp(App).mount('#app')
